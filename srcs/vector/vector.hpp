8db9ae040a1126ac4e3fbcc788f779193999438f
Alexis Ferrand <aleferra@z1r12p3.42lyon.fr>
2023-03-03 14:15:57 +0100

    vector members type

---
+++ srcs/vector/vector.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   vector.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aleferra <aleferra@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/03 13:36:17 by aleferra          #+#    #+#             */
+/*   Updated: 2023/03/03 14:14:51 by aleferra         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+template <class T> class Vector
+{
+    public :
+            typedef T value_type;
+            typedef value_type* pointer;
+            typedef value_type* iterator;
+            typedef const value_type* const_iterator;
+            typedef value_type& reference;
+            typedef const value_type& const_reference;
+            typedef size_t size_type;
+            typedef ptrdiff_t difference_type;
+    private :
+    
+}
